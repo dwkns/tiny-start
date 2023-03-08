5cad17d60390b8b1121faef2544c20a72d59e6b4
dwkns <dwkns@me.com>
2023-03-08 17:48:24 +0000

    Update config

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,24 +1,26 @@
-const { logToConsole } = require('dwkns-eleventy-plugins')
-// const { logToConsole, inlineSVG, htmlMinifer, readableDate, prependAnOrA } = require('../dwkns-eleventy-plugins') // local version
-const pluginWebc = require("@11ty/eleventy-plugin-webc");
-module.exports = (eleventyConfig) => {
+const {
+  logToConsole,
+  // inlineSVG,
+  // htmlMinifer,
+  // readableDate,
+  // prependAnOrA,
+} = require('dwkns-eleventy-plugins')
 
-  eleventyConfig.addPlugin(pluginWebc);
-  
+module.exports = (eleventyConfig) => {
   eleventyConfig.addPlugin(logToConsole, {
     logToHtml: true,
     logToConsole: false,
     colorizeConsole: true,
   });
 
-  // watch our script folder for changes. 
+  // watch our script folder for changes.
   eleventyConfig.addWatchTarget("./src/scripts/");
   eleventyConfig.addWatchTarget("./tailwind.config.js");
 
   eleventyConfig.addPassthroughCopy({
-    'src/fonts': './fonts',
-    'src/images': './images',
-    'src/styles/compiled.css': './styles/compiled.css',
+    "src/fonts": "./fonts",
+    "src/images": "./images",
+    "src/styles/compiled.css": "./styles/compiled.css",
   });
 
   eleventyConfig.setDataDeepMerge(true);
@@ -26,17 +28,17 @@ module.exports = (eleventyConfig) => {
   eleventyConfig.setServerOptions({
     domdiff: false, // reload instead of domdiff
     port: 8080,
-    showAllHosts: false, 
+    showAllHosts: false,
     showVersion: false,
   });
 
   return {
     dir: {
-      input: 'src',
-      output: 'dist',
-      includes: '_partials/',
-      layouts: '_partials/_layouts',
-      data: '_data',
+      input: "src",
+      output: "dist",
+      includes: "_partials/",
+      layouts: "_partials/_layouts",
+      data: "_data",
     },
   };
 };
